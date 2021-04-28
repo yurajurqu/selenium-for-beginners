0587b988bc912d9fc6c3e993c292c5e9e3775913
obarboza <obarboza@pe.ibm.com>
2021-04-28 08:45:48 -0500

    combining login tests

--- src/test/java/com/herokuapp/theinternet/LoginTests.java
+++ src/test/java/com/herokuapp/theinternet/LoginTests.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class PositiveTests {
+public class LoginTests {
 
-  @Test
-  public void loginTest() {
+  @Test(priority=1,groups = { "positiveTests", "smokeTests" })
+  public void positiveLoginTest() {
     System.out.println("Starting loginTest");
     // create driver
     System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
@@ -48,8 +50,42 @@ public class PositiveTests {
     //close browser
     driver.quit();
   }
-  
 
+  @Test(priority=2,groups = { "negativeTests", "smokeTests" })
+  @Parameters({"username","password","expectedMessage"})
+  public void negativeLoginTest(String user, String pass, String expectedMessage) {
+    System.out.println("Starting wongUsenameLoginTest");
+    // create driver
+    System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+    WebDriver driver = new FirefoxDriver();
+    // open test page
+    String url = "http://the-internet.herokuapp.com/login";
+    driver.get(url);
+    System.out.println("Page is opened.");
+
+    // sleep for 3 seconds
+    // sleep(3000);
+
+    // maximize browser window
+    driver.manage().window().maximize();
+    // enter username
+    WebElement username = driver.findElement(By.id("username"));
+    username.sendKeys(user);
+    // enter password
+    WebElement password = driver.findElement(By.id("password"));
+    password.sendKeys(pass);
+    // click login button
+    WebElement loginButton = driver.findElement(By.cssSelector("button"));
+    loginButton.click();
+    // verifications:
+
+    // wrong login message
+    WebElement errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+    Assert.assertTrue(errorMessage.getText().contains(expectedMessage));
+
+    //close browser
+    driver.quit();
+  }
   private void sleep(long t) {
     try {
       Thread.sleep(t);
